47e3957b79a53e491a83ebc7b82f6715a1566fbb
thisisbhanuj <tobhanuj@gmail.com>
2024-09-02 14:13:39 +1000

    26. Remove Duplicates from Sorted Array

---
+++ src/main/ds/leetcode/easy/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,21 @@
+package main.ds.leetcode.easy;
+
+// Given an integer array nums sorted in non-decreasing order,
+// remove the duplicates in-place such that each unique element appears only once.
+// The relative order of the elements should be kept the same.
+// Then return the number of unique elements in nums.
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int uniqueIndex = 1;
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i=1]){
+                nums[uniqueIndex] = nums[i];
+                uniqueIndex++;
+            }
+        }
+
+        return uniqueIndex;
+    }
+}
